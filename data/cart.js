bcb20d6ae100c5af775f563fb93d0f5624f17aa5
riyaverma <techriya054@gmail.com>
2025-09-19 14:39:53 +0530

    generated cart product's html

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cartItem = []
+export const cartItem = [
+    {
+         id: "PRD-A15P-9X7Q4",
+         quantity:2,
+    },
+    {
+        id: "PRD-S24U-8K3R2",
+        quantity: 1,
+    }
+]
 //dynamically changing the number of items in the cart which is shown on the top of the cart
 export const updateCartQuantity = () => {
 
---
+++ script/checkout.js
@@ -0,0 +1,52 @@
+import { cartItem } from '../data/cart.js';
+import { products } from '../data/product.js';
+
+const orderContainer = document.querySelector('.order-container');
+let cartSummaryHtml = ``;
+
+
+
+cartItem.forEach((product)=>{
+    let productId = product.id;
+    let matchingItem ;
+    products.forEach((productItem)=>{
+        if(productItem.id === productId){
+            matchingItem = productItem;
+        }
+
+     })
+
+    cartSummaryHtml += `
+     <div class="delivery-date">Delivery date : Tuesday, june 21
+
+                    </div>
+                    <div class="order-info">
+                        <div class="order-product-image">
+                            <img src="${matchingItem.img}" alt="" srcset="">
+
+                        </div>
+                        <div class="order-product-info">
+                            <p class="order-product-name">${matchingItem.name}</p>
+                            <p class="order-product-price">$ ${(matchingItem.priceCents /100).toFixed(2)}</p>
+                            <p class="order-product-quantity">quantity: ${product.quantity}</p>
+                        </div>
+                        <div class="order-delivery-info">
+                            <div class="order-delivery-heading">
+                                choose a delivery date
+                            </div>
+                            <div class="delivery-date-form">
+                                <div class="delivery-date-select"><input type="radio" name="delivery-date" id="delivery-date">after 2 days</div>
+                                
+                               <div class="delivery-date-select"> <input type="radio" name="delivery-date" id="delivery-date">
+                                after 4days</div>
+                               <div class="delivery-date-select"><input type="radio" name="delivery-date" id="delivery-date">
+                                after 6 days</div>
+                                
+                            </div>
+
+
+                        </div>
+                    </div>
+    `
+})
+orderContainer.innerHTML = cartSummaryHtml
